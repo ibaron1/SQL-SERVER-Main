c24b2f9f5323aeb343c640513fbae35d08d03918
ibaron1 <ibaron1@msn.com>
2018-08-16 16:13:41 -0400

    Add files via upload

---
+++ Disable, enable trigger.sql
@@ -0,0 +1,8 @@
+DISABLE TRIGGER dbo.tI_ChangeLog on dbo.ChangeLog
+
+ENABLE TRIGGER dbo.tI_ChangeLog on dbo.ChangeLog
+
+
+DISABLE TRIGGER dbo.tD_ChangeLog on dbo.ChangeLog
+
+ENABLE TRIGGER dbo.tD_ChangeLog on dbo.ChangeLog
